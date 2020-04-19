29086e44065458dbfd46cb546a137906b019d0c9
dbrdarski <dane.brdarski@gmail.com>
2020-04-20 00:10:37 +0200

    milestone

--- ms.js
+++ ms.js
@@ -2,13 +2,13 @@ const traverse = require("@babel/traverse");
 const id = x => x;
 
 const applyHandlers = (node, handlers = []) => (value, index) => (handlers[index] || id)(node[value]);
-const helper = (block) => console.log(block) || block
-
-const getBinding = (scope, binding) => scope.bindings[binding] || (scope.parent ? getBinding(scope.parent, binding) : null)
+const helper = (block) => console.log(block) || block;
+const getBinding = (scope, binding) => scope.bindings[binding] || (scope.parent ? getBinding(scope.parent, binding) : null);
 
 module.exports = function(babel) {
   var t = babel.types;
 
+  const getKey = (node) => node.computed ? node.property : t.stringLiteral(node.property.name);
   const mApply = (node, args, operator, handlers) => t.callExpression(
     t.identifier('M'),
     [t.stringLiteral(operator), ...args.map(applyHandlers(node, handlers)).filter(x => x != null)]
@@ -16,8 +16,6 @@ module.exports = function(babel) {
 
   const handler = (args, identifier, handlers) => function (path, state) {
     const operator = identifier || path.node.operator;
-    // console.log(state.file.scope.bindings)
-    // console.log(path.scope)
     path.replaceWith(mApply(path.node, args, operator, handlers));
   };
   const pipeNonNull = (...fns) => (val) => val == null ? null : fns.reduce((acc, fn) => fn(acc), val);
@@ -49,13 +47,7 @@ module.exports = function(babel) {
       // },
       UnaryExpression: { exit: unary },
       Identifier (path) {
-        // console.log(path.node.name, Object.keys(path.scope.bindings))
-        if (path.scope) {
-          console.log(path.scope.hasBinding(path.node.name), path.node.name);
-          const match = getBinding(path.scope, path.node.name);
-          // console.log({ match })
-          // console.log(path.node.name)
-        }
+        const match = getBinding(path.scope, path.node.name);
       },
       BinaryExpression: { exit: mostCommonExpression },
       LogicalExpression: { exit: mostCommonExpression },
@@ -63,24 +55,24 @@ module.exports = function(babel) {
       IfStatement: { exit: conditionalExpression },
       ExpressionStatement: {
         exit (path, state) {
-          const expression = path.node.expression
+          const expression = path.node.expression;
           if (expression.type === "AssignmentExpression" && expression.left.type === "MemberExpression") {
-            path.node.left = expression.left.object
-            path.node.middle = expression.left.property
-            path.node.right = expression.right
-            handler(['left', 'middle', 'right'], 'set')(path)
+            path.node.left = expression.left.object;
+            path.node.middle = getKey(expression.left);
+            path.node.right = expression.right;
+            handler(['left', 'middle', 'right'], 'set')(path);
           }
         }
       },
       MemberExpression: {
         exit (path) {
-          const assignment = path.parent.parentExpression;
-          if (assignment) {
-            assignment.replacementNode = path
-            handler(['replacementNode', 'right'], 'set')(assignment)
-          } else {
-            handler(['object', 'property'], 'get')(path)
+          const parent = path.parent;
+          if (parent.type === 'AssignmentExpression' && parent.left === path.node) {
+            // const assignment = path.findParent(path => path.isExpressionStatement());
+            return;
           }
+          path.node.right = getKey(path.node);
+          handler(['object', 'right'], 'get')(path);
         }
       }
     }
